6a4f82c3348a74afc96212e46a76ffd69075dab3
Adam Poper <64482829+AdamPoper@users.noreply.github.com>
2020-08-25 12:26:59 -0400

    Update Entity.h

--- renderer/Entity.h
+++ renderer/Entity.h
@@ -8,28 +8,31 @@
 #include "../glm/gtc/matrix_transform.hpp"
 #include <cmath>
 
-#define CIRCLE_TRIANGLES 16
+class IndexBuffer;
 
 namespace ap {
 
 	class Entity
 	{
+	private:
+		
 	public:
+		friend class IndexBuffer;
 		friend class Renderer;
+		friend class Quad;
+		friend class Triangle;
 		Entity();
-		virtual ~Entity();
-		// TODO there's no reason these have to be virtual will be changed later
-		virtual void setPosition(const Vec2f& pos) = 0;
-		virtual void setColor(const Vec4f& color)  = 0;
-		virtual void setColor(const Vec3f& color)  = 0;
-		virtual void setSize(const Vec2f& size)    = 0;
-		virtual size_t getNumIndicies()            = 0;
-		virtual size_t getNumVerticies()           = 0;
+		virtual ~Entity();   // always has a virtual destructor		
+		void setPosition(const Vec2f& pos);
+		void setColor(const Vec4f& color);
+		void setColor(const Vec3f& color);
+		void setSize(const Vec2f& size);
+		virtual const size_t getNumIndicies()  = 0;
+		virtual const size_t getNumVerticies() = 0;
 		virtual void setTexture(Texture*);
-		virtual void rotate(float degres)          = 0;
+		virtual void rotate(float degres)      = 0;
 		virtual const Texture* getTexture() const;
-		void move(const Vec2f& offset);
-		const glm::mat4& getModel() const;
+		void move(const Vec2f& offset);		
 		const Vec2f& getSize() const;
 		const Vec2f& getPosition() const;
 		const Vec4f& getColor() const;
@@ -38,6 +41,12 @@ namespace ap {
 	protected:
 		virtual void setData() = 0;
 		const float getTextureIndex() const;
+		enum IndexCount {
+			IndiciesTriangle = 3, IndiciesQuad = 6
+		};
+		enum VertexCount {
+			VerticiesTriangle = 3, VerticiesQuad = 4
+		};
 	protected:
 		std::vector<Vertex> m_verticies;
 		// chaches the pointers to textures in case an entity has the same texture as another entity
@@ -46,22 +55,17 @@ namespace ap {
 		Vec2f m_position;
 		Vec4f m_color;
 		Vec2f m_size;
-		Texture* m_texture;
-		glm::mat4 m_model;
+		Texture* m_texture;		
 		static float s_nextTextureIndex;
 		bool m_hasTexture = false;
 	};
 	class Quad : public Entity
 	{
 	public:
-		Quad();
-		void setPosition(const Vec2f& pos) override;
-		void setColor(const Vec4f& color)  override;
-		void setColor(const Vec3f& color)  override;
-		void setSize(const Vec2f& size)    override;		
-		size_t getNumIndicies()            override;
-		size_t getNumVerticies()           override;
-		void rotate(float degrees)         override {}
+		Quad();			
+		const size_t getNumIndicies()  override;
+		const size_t getNumVerticies() override;
+		void rotate(float degrees)     override {}
 		static const size_t s_numIndicies;
 		static const size_t s_numVerticies;
 	private:
@@ -70,40 +74,14 @@ namespace ap {
 	class Triangle : public Entity
 	{
 	public:
-		Triangle();
-		void setPosition(const Vec2f& pos) override;
-		void setColor(const Vec4f& color)  override;
-		void setColor(const Vec3f& color)  override;
-		void setSize(const Vec2f& size)    override;		
-		size_t getNumIndicies()			   override;
-		size_t getNumVerticies()		   override;
-		void rotate(float degrees)         override;
+		Triangle();		
+		const size_t getNumIndicies()  override;
+		const size_t getNumVerticies() override;
+		void rotate(float degrees)     override;
 		static const size_t s_numIndicies;
 		static const size_t s_numVerticies;
 	private:
 		Vertex& at(uint32_t vertexIndex);		
 		void setData() override;
-
-	};
-	// the circle class is still under development which is why the class has methods that are incomplete
-	class Circle : public Entity
-	{
-	public:
-		Circle();
-		void setPosition(const Vec2f& pos) override;
-		void setColor(const Vec4f& color)  override;
-		void setColor(const Vec3f& color)  override;
-		void setSize(const Vec2f& size)    override {}  // will not be used so no reason to be virtual
-		void setRadius(float radius);
-		void rotate(float degrees)         override {}
-	    size_t getNumIndicies()			   override;
-	    size_t getNumVerticies()		   override;
-		static const size_t s_numIndicies;
-		static const size_t s_numVerticies;
-	private:
-		void setData() override;
-		float degreesToRadians(float deg) { return (deg * (3.14159f / 180.0f)); }
-		float m_radius;
-		std::array<Triangle, CIRCLE_TRIANGLES> m_triangles;
 	};	
 }
